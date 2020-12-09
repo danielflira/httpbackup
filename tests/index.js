0f2258ddd06c91a6175af58bd6082e5e97b130db
daniel.fonseca <daniel.fonseca@totvs.com.br>
2020-12-09 15:07:05 -0300

    ajusta mensagem dos testes

--- tests/index.js
+++ tests/index.js
@@ -11,7 +11,7 @@ test("Base backend storeData shoud throw NotImplemented", async (t) => {
 
     try {
         await backend.storeData();
-        t.fail();
+        t.fail("storeData from base class has implementation?");
     } catch(e) {
         t.pass();
     }
@@ -22,7 +22,7 @@ test("Base backend retriveData shoud throw NotImplemented", async (t) => {
 
     try {
         await backend.retriveData();
-        t.fail();
+        t.fail("retriveData from base class has implementation?");
     } catch(e) {
         t.pass();
     }
@@ -33,7 +33,7 @@ test("Base backend retriveMeta shoud throw NotImplemented", async (t) => {
 
     try {
         await backend.retriveMeta();
-        t.fail();
+        t.fail("retriveMeta from base class has implementation?");
     } catch(e) {
         t.pass();
     }
@@ -44,7 +44,7 @@ test("Base backend storeMeta shoud throw NotImplemented", async (t) => {
 
     try {
         await backend.storeMeta();
-        t.fail();
+        t.fail("storeMeta from base class has implementation?");
     } catch(e) {
         t.pass();
     }
@@ -54,7 +54,7 @@ test("Transform virtualpath to datarealpath of FileSystemBackend", async (t) => 
     let backend = new FileSystemBackend();
 
     if ( backend.dataPath("abcdef0123456789") !== "store/data/ab/abcdef0123456789" ) {
-        t.fail();
+        t.fail('backend.dataPath("abcdef0123456789") !== "store/data/ab/abcdef0123456789"');
     }
 
     t.pass();
@@ -64,15 +64,15 @@ test("Transform virtualpath to metarealpath of FileSystemBackend", async (t) => 
     let backend = new FileSystemBackend();
 
     if ( backend.metaPath("/primeiro//caminho/") !== "store/meta/primeiro/caminho" ) {
-        t.fail();
+        t.fail('backend.metaPath("/primeiro//caminho/") !== "store/meta/primeiro/caminho"');
     }
 
     if ( backend.metaPath("\\primeiro\\caminho") !== "store/meta/primeiro/caminho" ) {
-        t.fail();
+        t.fail('backend.metaPath("\\primeiro\\caminho") !== "store/meta/primeiro/caminho"');
     }
 
     if ( backend.metaPath("\\segundo/caminho\\de//teste") !== "store/meta/segundo/caminho/de/teste" ) {
-        t.fail();
+        t.fail('backend.metaPath("\\segundo/caminho\\de//teste") !== "store/meta/segundo/caminho/de/teste"');
     }
 
     t.pass();
@@ -86,19 +86,19 @@ test("Random path generation of FileSystemBackend", async (t) => {
     let path3 = backend.parsePath(backend.tempPath("teste"));
 
     if ( path1.dirName !== "store/temp" ) {
-        t.fail();
+        t.fail('path1.dirName !== "store/temp"');
     }
 
     if ( path2.dirName !== ("store/temp") ) {
-        t.fail();
+        t.fail('path2.dirName !== ("store/temp")');
     }
 
     if ( path1.baseName === path2.baseName ) {
-        t.fail();
+        t.fail('path1.baseName === path2.baseName');
     }
 
     if ( path3.baseName !== "teste" ) {
-        t.fail();
+        t.fail('path3.baseName !== "teste"');
     }
 
     t.pass();
@@ -110,15 +110,15 @@ test("Parse paths of FileSystemBackend", async (t) => {
     let {filePath, dirName, baseName} = backend.parsePath("store/data/primeiro/path/para/arquivo1");
 
     if ( filePath !== "store/data/primeiro/path/para/arquivo1" ) {
-        t.fail();
+        t.fail('filePath !== "store/data/primeiro/path/para/arquivo1"');
     }
 
     if ( dirName !== "store/data/primeiro/path/para" ) {
-        t.fail();
+        t.fail('dirName !== "store/data/primeiro/path/para"');
     }
 
     if ( baseName !== "arquivo1" ) {
-        t.fail()
+        t.fail('baseName !== "arquivo1"');
     }
 
     t.pass();
@@ -132,8 +132,8 @@ test("Make recursive directories of FileSystemBackend", async (t) => {
 
     let stats = await Fs.promises.stat(filePath)
 
-    if ( ! stats.isDirectory() ) {
-        t.fail()
+    if ( stats.isDirectory() !== true ) {
+        t.fail('stats.isDirectory() !== true');
     }
 
     // cleanup
@@ -148,11 +148,11 @@ test("Create tempfile of FileSystemBackend", async (t) => {
     let {tempID, md5, sha512} = await backend.storeTemp(Stream.Readable.from("conteudo do arquivo\n"));
 
     if ( sha512 !== "adc91ac132e584d3ea4fd7b9d0bc393af6fa5a69df8a854318a4739405dd86204e1bdc9235810cce7fe2822c34fe515d8c4313028f287f2684d74f9a6fa566c7" ) {
-        t.fail();
+        t.fail('sha512 !== "adc91ac132e5 ... fa566c7"');
     }
 
     if ( md5 !== "159033dce20972eb518295770fd34f60" ) {
-        t.fail();
+        t.fail('md5 !== "159033dce20972eb518295770fd34f60"');
     }
 
     await backend.removeTemp(tempID);
@@ -170,13 +170,15 @@ test("retrive meta of FileSystemBackend", async (t) => {
     } catch(e) {}
 
     if ( (await backend.retriveMeta(filePath)).length !== 0 ) {
-        t.fail();
+        // aparentemente um meta dado inexistente retornou resultado
+        t.fail('await backend.retriveMeta(filePath)).length !== 0');
     }
 
     await backend.storeMeta(filePath, {campo1: "valor1"});
 
     if ( (await backend.retriveMeta(filePath)).campo1 !== "valor1" ) {
-        t.fail();
+        // aparentemente um meta dado existente não retornou resultado esparado
+        t.fail('(await backend.retriveMeta(filePath)).campo1 !== "valor1"');
     }
 
     t.pass();
@@ -194,7 +196,8 @@ test("store meta of FileSystemBackend", async (t) => {
     await backend.storeMeta(filePath, {conteudo: "funciona!"});
 
     if ( (await backend.retriveMeta(filePath)).conteudo !== "funciona!" ) {
-        t.fail();
+        // nao foi recuperado o valor esperado
+        t.fail('(await backend.retriveMeta(filePath)).conteudo !== "funciona!"');
     }
 
     t.pass()
@@ -211,25 +214,25 @@ test("store data of FileSystemBackend", async (t) => {
     let result = await backend.storeData(filePath, Stream.Readable.from("conteudo 1 do arquivo\n"));
 
     if ( result.newFile !== true && result.updateMeta !== true ) {
-        t.fail();
+        t.fail('result.newFile !== true && result.updateMeta !== true');
     }
 
     result = await backend.storeData(filePath, Stream.Readable.from("conteudo 1 do arquivo\n"));
 
     if ( result.newFile !== false && result.updateMeta !== false ) {
-        t.fail();
+        t.fail('result.newFile !== false && result.updateMeta !== false');
     }
 
     result = await backend.storeData(filePath, Stream.Readable.from("conteudo 2 do arquivo\n"));
 
     if ( result.newFile !== true && result.updateMeta !== true ) {
-        t.fail();
+        t.fail('result.newFile !== true && result.updateMeta !== true');
     }
 
     result = await backend.storeData(filePath, Stream.Readable.from("conteudo 1 do arquivo\n"));
 
     if ( result.newFile !== false && result.updateMeta !== true ) {
-        t.fail();
+        t.fail('result.newFile !== false && result.updateMeta !== true');
     }
 
     t.pass();
@@ -242,7 +245,7 @@ test("retrive data of FileSystemBackend", async (t) => {
     let result = await backend.storeData(filePath, Stream.Readable.from("conteudo 4 do arquivo 4\n"));
 
     if ( result.newFile !== true && result.updateMeta !== true ) {
-        t.fail();
+        t.fail('result.newFile !== true && result.updateMeta !== true');
     }
 
     const validateStream = new Stream.Writable({
@@ -260,7 +263,7 @@ test("retrive data of FileSystemBackend", async (t) => {
     await backend.retriveData(filePath, validateStream);
 
     if ( validateStream.data !== "conteudo 4 do arquivo 4\n" ) {
-        t.fail("o conteúdo do arquivo não bate");
+        t.fail('validateStream.data !== "conteudo 4 do arquivo 4\n"');
     }
 
     t.pass();
@@ -286,7 +289,7 @@ test("retrive data tha does not exists of FileSystemBackend", async (t) => {
     let result = await backend.retriveData(filePath, validateStream);
 
     if ( result.found !== false ) {
-        t.fail("file that does not exists was found?");
+        t.fail('result.found !== false');
     }
     
     t.pass();
